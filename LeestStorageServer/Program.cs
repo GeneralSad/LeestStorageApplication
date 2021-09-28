7d89d35a83dd3f93431842bd8ded826c5394e49b
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-09-28 11:28:34 +0200

    Made server project

---
+++ LeestStorageServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeestStorageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
