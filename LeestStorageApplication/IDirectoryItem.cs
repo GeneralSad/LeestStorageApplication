2864e5d074480a22331c8b580675daab07d58478
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-10-05 11:01:34 +0200

    Made FileDir classes

---
+++ LeestStorageApplication/IDirectoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeestStorageApplication
+{
+    interface IDirectoryItem
+    {
+
+        public string name { get; set; }
+        public Folder parent { get; set; }
+        public string getFilePath();
+
+    }
+}
--- LeestStorageApplication/MainWindow.xaml.cs
+++ LeestStorageApplication/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using LeestStorageServer;
 
@@ -11,6 +16,22 @@ namespace LeestStorageApplication
     {
         public MainWindow()
         {
+
+            //Client client = new Client(25565);
+
+            Folder folder1 = new Folder("hell", null);
+            Folder folder2 = new Folder("yeah", folder1);
+            Folder folder3 = new Folder("naw", folder1);
+            Folder folder4 = new Folder("man", folder2);
+
+            File file1 = new File("ThatsRad.txt", folder2); //Hell/Yeah
+            File file2 = new File("ThatsNotSoRad.txt", folder3); //Hell/Naw
+            File file3 = new File("ThatsRad.txt", folder4); //Hell/Yeah/Man
+
+            Debug.WriteLine("File 1: " + file1.getFilePath());
+            Debug.WriteLine("File 2: " + file2.getFilePath());
+            Debug.WriteLine("File 3: " + file3.getFilePath());
+
             CommunicationHandler handler = new CommunicationHandler();
             InitializeComponent();
             
