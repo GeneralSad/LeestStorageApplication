4eb3317843630cdd351191b3cc378fc6e84b4dab
Leon van der Sar <leon.v.d.sar@gmail.com>
2021-10-05 15:01:02 +0200

    Started using MVVM

--- LeestStorageApplication/MainWindow.xaml.cs
+++ LeestStorageApplication/MainWindow.xaml.cs
@@ -14,40 +14,21 @@ namespace LeestStorageApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly ViewModel viewModel;
+
         public MainWindow()
         {
 
-            Folder folder1 = new Folder("hell", null);
-            Folder folder2 = new Folder("yeah", folder1);
-            Folder folder3 = new Folder("naw", folder1);
-            Folder folder4 = new Folder("man", folder2);
-
-            File file1 = new File("ThatsRad.txt", folder2); //Hell/Yeah
-            File file2 = new File("ThatsNotSoRad.txt", folder3); //Hell/Naw
-            File file3 = new File("ThatsRad.txt", folder4); //Hell/Yeah/Man
-
-            Debug.WriteLine("File 1: " + file1.getFilePath());
-            Debug.WriteLine("File 2: " + file2.getFilePath());
-            Debug.WriteLine("File 3: " + file3.getFilePath());
-
-
-            List<Item> Items = new List<Item>{
-                new Item{Name= "Hell", Path=folder1.getFilePath()},
-                new Item{Name= "Yeah", Path=folder2.getFilePath()},
-                new Item{Name= "Naw", Path=folder3.getFilePath()},
-                new Item{Name= "ThatsRad.txt", Path=file3.getFilePath()}
-            };
-
-            CommunicationHandler handler = new CommunicationHandler();
+            //CommunicationHandler handler = new CommunicationHandler();
             InitializeComponent();
-            
-        }
-    }
+            viewModel = new ViewModel();
+            this.DataContext = viewModel;
 
-    class Item
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
+        }
+
+
+
     }
 
 }
--- LeestStorageApplication/ViewModel.cs
+++ LeestStorageApplication/ViewModel.cs
@@ -1,14 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LeestStorageApplication
 {
-    //class ViewModel : INotifyPropertyChanged
-    //{
-    //    public event PropertyChangedEventHandler PropertyChanged;
-    //}
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<Item> items;
+
+        public ViewModel()
+        {
+            Folder folder1 = new Folder("hell", null);
+            Folder folder2 = new Folder("yeah", folder1);
+            Folder folder3 = new Folder("naw", folder1);
+            Folder folder4 = new Folder("man", folder2);
+
+            File file1 = new File("ThatsRad.txt", folder2); //Hell/Yeah
+            File file2 = new File("ThatsNotSoRad.txt", folder3); //Hell/Naw
+            File file3 = new File("ThatsRad.txt", folder4); //Hell/Yeah/Man
+
+            Debug.WriteLine("File 1: " + file1.getFilePath());
+            Debug.WriteLine("File 2: " + file2.getFilePath());
+            Debug.WriteLine("File 3: " + file3.getFilePath());
+
+            items = new List<Item>();
+            items.Add(new Item { Name = "Hell", Path = folder1.getFilePath() });
+            items.Add(new Item { Name = "Yeah", Path = folder2.getFilePath() });
+            items.Add(new Item { Name = "Naw", Path = folder3.getFilePath() });
+            items.Add(new Item { Name = "ThatsRad.txt", Path = file3.getFilePath() });
+        }
+
+        public List<Item> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                OnPropertyChange("DirectoryListView");
+            }
+        }
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
 }
